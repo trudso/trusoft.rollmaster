6269504bc078aa461033339352d0eed3bdc19e23
Morten Trudsø <trudso@gmail.com>
2024-11-17 09:16:48 +0100

    Spiking on model and basic layout

---
+++ Src/Systems.WorldOfDarkness/Views/Components/ICharacterSheetModel.cs
@@ -0,0 +1,9 @@
+﻿using System.WorldOfDarkness;
+
+namespace Systems.WorldOfDarkness.Views.Components;
+
+public interface ICharacterSheetModel
+{
+    string Name { get; set; }
+    WodSubSystem SubSystem { get; set; }
+}
---
+++ Src/Systems.WorldOfDarkness/Views/Components/VampireCharacterSheetModel.cs
@@ -0,0 +1,9 @@
+﻿using System.WorldOfDarkness;
+
+namespace Systems.WorldOfDarkness.Views.Components;
+
+public class VampireCharacterSheetModel : ICharacterSheetModel
+{
+    public string Name { get; set; }
+    public WodSubSystem SubSystem { get; set;  }
+}
